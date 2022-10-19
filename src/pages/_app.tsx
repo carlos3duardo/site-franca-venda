91de460f02cfa5cbabe3e156e9bba033e8d33190
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2022-10-19 00:41:03 -0300

    Adicionando um visualizador de breakpoint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import theme from '../styles/themes/default';
+import ViewBreakpoint from '../components/Layout/ViewBreakpoint';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
         <title>Franca Venda</title>
       </Head>
       <Component {...pageProps} />
+      <ViewBreakpoint />
     </ChakraProvider>
   );
 }
